743347f45dc686175b71096b84012f8ddd03494d
Pedro Pinheiro <pepeugames2003@gmail.com>
2026-04-24 01:36:44 -0300

    Migrations feita

---
+++ src/database/migrations/2026_04_24_035707_create_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf', 45)->unique() ;
+            $table->string('nome', 45);
+            $table->date('data_nascimento');
+            $table->text('biografia')->nullable();
+            $table->string('genero', 10)->nullable();
+            $table->string('nacionalidade', 45)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
