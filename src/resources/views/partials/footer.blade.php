349f4aa096c920ab801c48b78eabb7e5b8ba1c3d
Pedro Pinheiro <pepeugames2003@gmail.com>
2026-05-14 19:32:20 -0300

    Header Base

---
+++ src/resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="container-fluid footer mt-auto fixed-bottom align-content-center">
+  <div class="container">
+    <div class="d-flex flex-wrap justify-content-between align-items-center" >
+      <p class="text-body-secondary mb-0 h5">IMDp</p>
+      <ul class="nav justify-content-end flex-row flex-wrap gap-3 align-items-center">
+        <li class="nav-item"><a href="{{ route('home') }}" class="nav-link px-2">Página Inicial</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -1,29 +1,39 @@
-<nav class="navbar navbar-expand-lg">
-  <div class="container">
+<nav class="navbar navbar-expand-sm align-content-center">
+  <div class="container-fluid">
     <a class="navbar-brand fw-bold" href="/">IMDp</a>
-    <ul class="navbar-nav ms-auto">
-      <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" aria-current="page"
-          href="{{ route('home') }}">
-          Início
-        </a>
-      </li>
-      <button id="toggle-theme" class="botao-cor" aria-label="Alternar tema">
-        <span class="icon-wrapper">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill"
-            viewBox="0 0 16 16" id="icone-escuro">
-            <path
-              d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-sun-fill hiddenNavbar" viewBox="0 0 16 16" id="icone-claro">
-            <path
-              d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
-          </svg>
-        </span>
-      </button>
-      </li>
 
-  </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <ul class="navbar-nav flex-row flex-wrap gap-3 align-items-center" >
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">Início</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Filmes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pessoas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gêneros</a>
+        </li>
+        <li class="nav-item">
+          <button aria-label="Login ou cadastro de usuário" class="botao">
+            <i class="bi bi-person-circle fs-4"></i>
+            <i class="bi bi-person-circle fs-4 hiddenNavbar"></i>
+          </button>
+        </li>
+          <button id="toggle-theme" aria-label="Alternar tema">
+            <i class="bi bi-moon-fill fs-4" id="icone-escuro"></i>
+            <i class="bi bi-sun-fill hiddenNavbar fs-4" id="icone-claro"></i>
+          </button>
+        </li>
+
+      </ul>
+    </div>
   </div>
 </nav>
